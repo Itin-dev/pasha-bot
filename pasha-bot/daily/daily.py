4fc8bf39c9092eea8c2f0d33f6d2027ae28c998b
Stan Makov <makovstpm@gmail.com>
2024-10-31 22:54:04 +0100

    chore: run daily.py from main.py

--- pasha-bot/daily/daily.py
+++ pasha-bot/daily/daily.py
@@ -68,10 +68,10 @@ def run_async_task():
 
 # Set up the scheduler to run the task at 07:10 AM every day
 scheduler = BlockingScheduler(timezone=LOCAL_TZ)
-scheduler.add_job(run_async_task, 'cron', hour=23, minute=30)
+scheduler.add_job(run_async_task, 'cron', hour=22, minute=50)
 
-# Start the scheduler
-if __name__ == "__main__":
+# Function to start the scheduler, used in main.py
+def run_daily_scheduler():
     logger.info("Starting the daily summary scheduler")
     try:
         scheduler.start()
